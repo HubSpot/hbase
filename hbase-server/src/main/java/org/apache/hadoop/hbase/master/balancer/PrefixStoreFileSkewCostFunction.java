ee04a90f1c37ceb0ff1437c9c32279369078d6fa
Elias Szabo <eszabo@hubspot.com>
2025-04-03 16:30:41 -0400

    Add log

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/PrefixStoreFileSkewCostFunction.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/PrefixStoreFileSkewCostFunction.java
@@ -1,26 +1,28 @@
 package org.apache.hadoop.hbase.master.balancer;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.yetus.audience.InterfaceAudience;
 
-@InterfaceAudience.Private
-public class PrefixStoreFileSkewCostFunction extends CostFromPrefixLoadFunction {
+@InterfaceAudience.Private public class PrefixStoreFileSkewCostFunction
+  extends CostFromPrefixLoadFunction {
+  private static final Logger LOG = LoggerFactory.getLogger(PrefixStoreFileSkewCostFunction.class);
 
   private static final String STOREFILE_SIZE_COST_KEY =
     "hbase.master.balancer.stochastic.prefixStoreFileSkewCost";
   private static final float DEFAULT_STOREFILE_SIZE_COST = 50;
 
   PrefixStoreFileSkewCostFunction(Configuration conf) {
-    this.setMultiplier(
-        conf.getFloat(STOREFILE_SIZE_COST_KEY, DEFAULT_STOREFILE_SIZE_COST)
-      );
+    this.setMultiplier(conf.getFloat(STOREFILE_SIZE_COST_KEY, DEFAULT_STOREFILE_SIZE_COST));
 
     // TODO: remove this so we use what's in config
     this.setMultiplier(50);
+
+    LOG.info("Initialized PrefixStoreFileSkewCostFunction");
   }
 
-  @Override
-  protected double getCostFromRl(BalancerRegionLoad rl) {
+  @Override protected double getCostFromRl(BalancerRegionLoad rl) {
     return rl.getStorefileSizeMB();
   }
 }
