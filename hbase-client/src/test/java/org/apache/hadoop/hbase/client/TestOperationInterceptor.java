9fdcb127fb3e227e1dded4129ec295fe9e265d9f
Ray Mattingly <rmdmattingly@gmail.com>
2025-06-30 09:59:03 -0400

    HubSpot Backport: HBASE-29351 Quotas: adaptive wait intervals (drop in 2.7)
    Co-authored-by: Ray Mattingly <rmattingly@hubspot.com>

--- hbase-client/src/test/java/org/apache/hadoop/hbase/client/TestOperationInterceptor.java
+++ hbase-client/src/test/java/org/apache/hadoop/hbase/client/TestOperationInterceptor.java
@@ -17,29 +17,23 @@
  */
 package org.apache.hadoop.hbase.client;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.fail;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.fail;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseClassTestRule;
 import org.apache.hadoop.hbase.testclassification.ClientTests;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-@Category({ ClientTests.class, SmallTests.class })
+@Tag(ClientTests.TAG)
+@Tag(SmallTests.TAG)
 public class TestOperationInterceptor {
-
-  @ClassRule
-  public static final HBaseClassTestRule CLASS_RULE =
-    HBaseClassTestRule.forClass(TestOperationInterceptor.class);
 
   private static class MockOperationInterceptor extends OperationInterceptor {
     final List<String> events = new ArrayList<>();
