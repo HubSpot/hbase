52125db4aa03ba93d3e1541263addcf501bf3b3f
Tanya Stickles <tstickles@hubspot.com>
2026-09-01 14:25:47 -0400

    more readable this way

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/assignment/AssignmentManager.java
@@ -735,13 +735,6 @@ public class AssignmentManager {
     return serverNode.getSystemRegionInfoList();
   }
 
-  private static void checkNotSplitParent(RegionStateNode regionNode)
-    throws DoNotRetryRegionException {
-    if (regionNode.isSplit()) {
-      throw new DoNotRetryRegionException(
-        regionNode.getRegionInfo().getEncodedName() + " is a split parent and cannot be assigned");
-    }
-  }
 
   private void preTransitCheck(RegionStateNode regionNode, RegionState.State[] expectedStates)
     throws HBaseIOException {
@@ -749,7 +742,13 @@ public class AssignmentManager {
       throw new HBaseIOException(
         regionNode + " is currently in transition; pid=" + regionNode.getProcedure().getProcId());
     }
-    checkNotSplitParent(regionNode);
+    if (regionNode.isSplit()) {
+      throw new DoNotRetryRegionException(
+        regionNode.getRegionInfo().getEncodedName
+          () + " is a split parent and cannot be assigned")
+        ;
+    }
+
     if (!regionNode.isInState(expectedStates)) {
       throw new DoNotRetryRegionException(UNEXPECTED_STATE_REGION + regionNode);
     }
@@ -771,7 +770,13 @@ public class AssignmentManager {
     RegionStateNode regionNode = regionStates.getOrCreateRegionStateNode(regionInfo);
     regionNode.lock();
     try {
-      checkNotSplitParent(regionNode);
+      if (regionNode.isSplit()) {
+        throw new DoNotRetryRegionException(
+          regionNode.getRegionInfo().getEncodedName
+            () + " is a split parent and cannot be assigned")
+          ;
+      }
+
       if (override) {
         if (regionNode.getProcedure() != null) {
           regionNode.unsetProcedure(regionNode.getProcedure());
